6f57f565f8b9c62cfb4d90adfa2bea5459ed7b2c
Connor Johnson <connor.johnson@digital.cabinet-office.gov.uk>
2023-08-09 12:18:10 +0100

    OJ-1688: Added code to the lambda for fetching questions.

--- lambdas/fetch-questions/src/fetch-questions-handler.ts
+++ lambdas/fetch-questions/src/fetch-questions-handler.ts
@@ -1,8 +1,20 @@
 import { LambdaInterface } from "@aws-lambda-powertools/commons";
 
 export class FetchQuestionsHandler implements LambdaInterface {
-  public async handler(_event: unknown, _context: unknown): Promise<string> {
-    return "Hello, World!";
+  public async handler(event: any, _context: unknown): Promise<string> {
+
+    const response = await fetch(event.apiURL.value, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "User-Agent": event.userAgent.value,
+        Authorization: "Bearer " + event.oAuthToken.value,
+      },
+      body: JSON.stringify({
+        nino: event.nino,
+}),
+    });
+    return await response.json();
   }
 }
 
