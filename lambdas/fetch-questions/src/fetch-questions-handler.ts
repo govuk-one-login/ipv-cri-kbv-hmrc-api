bfc92ffa348d0fd26fb00d0ca97f3d5585e6d7e1
Connor Johnson <connor.johnson@digital.cabinet-office.gov.uk>
2023-08-09 14:16:34 +0100

    OJ-1688: Fixed incorrect event property name.

--- lambdas/fetch-questions/src/fetch-questions-handler.ts
+++ lambdas/fetch-questions/src/fetch-questions-handler.ts
@@ -3,11 +3,11 @@ import { LambdaInterface } from "@aws-lambda-powertools/commons";
 export class FetchQuestionsHandler implements LambdaInterface {
   public async handler(event: any, _context: unknown): Promise<string> {
 
-    const response = await fetch(event.paramters.url, {
+    const response = await fetch(event.parameters.url, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "User-Agent": event.paramters.userAgent,
+        "User-Agent": event.parameters.userAgent,
         Authorization: "Bearer " + event.oAuthToken.value,
       },
       body: JSON.stringify({
