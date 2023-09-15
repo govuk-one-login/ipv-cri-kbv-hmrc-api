902bab13b5e52fc425f53698db4af7c4f2d01520
Rahul Gupta <rahul.gupta@digital.cabinet-office.gov.uk>
2023-09-15 14:30:40 +0100

    added mocked jest tests

---
+++ integration-tests/tests/jest.config.ts
@@ -0,0 +1,12 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+import type { Config } from "@jest/types";
+import commonConfig from "./jest.config.common";
+
+const config: Config.InitialOptions = {
+  ...commonConfig,
+  testMatch: ["**/tests/**/*.test.ts"],
+};
+export default config;
