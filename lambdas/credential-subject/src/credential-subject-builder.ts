ae33f2a097ae91cd91c5bec4ebdbe866e2d997b7
Olakunle Jegede <olakunle.jegede@digital.cabinet-office.gov.uk>
2023-10-03 21:28:12 +0100

    OJ-1928: implement credential subject

---
+++ lambdas/credential-subject/src/credential-subject-builder.ts
@@ -0,0 +1,61 @@
+type Name = {
+  nameParts: Array<NamePart>;
+};
+
+export type BirthDate = {
+  value: string;
+};
+
+export type Address = {
+  streetName: string;
+  postalCode: string;
+  buildingNumber: string;
+  addressLocality: string;
+  validFrom: string;
+};
+
+export type NamePart = {
+  type: string;
+  value: string;
+};
+
+export type CredentialSubject = {
+  name: Array<Name>;
+  birthDate: Array<BirthDate>;
+  address: Array<Address>;
+};
+
+export class CredentialSubjectBuilder {
+  private name: Array<NamePart>;
+  private address: Array<Address>;
+  private birthDate: Array<BirthDate>;
+
+  constructor() {}
+
+  addName(type: string, value: string): CredentialSubjectBuilder {
+    this.name.push({ type, value } as NamePart);
+    return this;
+  }
+  addNames(names: Array<NamePart>) {
+    this.name = names;
+    return this;
+  }
+  setBirthDate(birthDates: Array<BirthDate>): CredentialSubjectBuilder {
+    this.birthDate = birthDates;
+    return this;
+  }
+
+  setAddresses(addreses: Array<Address>): CredentialSubjectBuilder {
+    this.address = addreses;
+    return this;
+  }
+  build(): CredentialSubject {
+    const name = [{ nameParts: this.name } as Name];
+
+    return {
+      name,
+      birthDate: this.birthDate,
+      address: this.address,
+    } as CredentialSubject;
+  }
+}
