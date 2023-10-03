a4345cc378d2af1e58f3717b7b9e97ab1ca1f6fd
surajkumar <suraj.kumar@digital.cabinet-office.gov.uk>
2023-10-03 15:23:52 +0100

    OJ-1887: Moved looping logic into Lambda so that it updates `score` column in QuestionsTable

--- lambdas/submit-answer/src/submit-answer-handler.ts
+++ lambdas/submit-answer/src/submit-answer-handler.ts
@@ -1,30 +1,63 @@
 import { LambdaInterface } from "@aws-lambda-powertools/commons";
+import { DynamoDBDocumentClient, UpdateCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { fromEnv } from "@aws-sdk/credential-providers";
+import { Logger } from "@aws-lambda-powertools/logger";
+
+const client = new DynamoDBClient({
+  region: process.env["AWS_REGION"],
+  credentials: fromEnv(),
+});
+
+const docClient = DynamoDBDocumentClient.from(client);
+
+const logger = new Logger();
 
 export class SubmitAnswerHandler implements LambdaInterface {
   public async handler(event: any, _context: unknown): Promise<string> {
-    const answers = event.questions.Items.map((question: any) => {
-      return {
-        questionKey: question.questionKey.S,
-        answer: question.answer.S,
-      };
-    });
-
-    const response = await fetch(event.parameters.url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "User-Agent": event.parameters.userAgent,
-        Authorization: "Bearer " + event.oAuthToken.value,
-      },
-      body: JSON.stringify({
-        correlationId: event.questions.Items[0].correlationId.S,
-        selection: {
-          nino: event.nino,
+    try {
+      const answers = event.questions.Items.map((question: any) => {
+        return {
+          questionKey: question.questionKey.S,
+          answer: question.answer.S,
+        };
+      });
+      const response = await fetch(event.parameters.url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "User-Agent": event.parameters.userAgent,
+          Authorization: "Bearer " + event.oAuthToken.value,
         },
-        answers: answers,
-      }),
-    });
-    return await response.json();
+        body: JSON.stringify({
+          correlationId: event.questions.Items[0].correlationId.S,
+          selection: {
+            nino: event.nino,
+          },
+          answers: answers,
+        }),
+      });
+      const json = await response.json();
+      for (const item of json) {
+        await docClient.send(
+          new UpdateCommand({
+            TableName: event.questionTableName,
+            Key: {
+              sessionId: event.sessionId,
+              questionKey: item.questionKey,
+            },
+            UpdateExpression: "SET score = :score",
+            ExpressionAttributeValues: {
+              ":score": item.score,
+            },
+          })
+        );
+      }
+      return json;
+    } catch (error: any) {
+      logger.error("SubmitAnswerHandler Error: " + error.message);
+      throw error;
+    }
   }
 }
 
