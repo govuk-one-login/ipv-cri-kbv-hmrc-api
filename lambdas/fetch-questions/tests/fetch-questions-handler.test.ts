b57252835de7fef838c2024f4faa43dd2e02bb5c
surajkumar <suraj.kumar@digital.cabinet-office.gov.uk>
2023-10-04 18:28:01 +0100

    OJ-1957: Added unit test for `fetch-questions-handler.test.ts`

--- lambdas/fetch-questions/tests/fetch-questions-handler.test.ts
+++ lambdas/fetch-questions/tests/fetch-questions-handler.test.ts
@@ -1,10 +1,103 @@
 import { FetchQuestionsHandler } from "../src/fetch-questions-handler";
 import { Context } from "aws-lambda";
 
+const smInput = {
+  parameters: {
+    url: "dummyUrl",
+    userAgent: "dummyUserAgent",
+  },
+  bearerToken: {
+    value: "dummyOAuthToken",
+  },
+  nino: "dummyNino",
+};
+
 describe("fetch-questions-handler", () => {
-  it("should print Hello, World!", async () => {
-    const fetchQuestionsHandler = new FetchQuestionsHandler();
-    const result = await fetchQuestionsHandler.handler({}, {} as Context);
-    expect(result).toStrictEqual("Hello, World!");
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe("Success Scenarios", async () => {
+    it("should return a set of questions for a valid nino", async () => {
+      global.fetch = jest.fn().mockImplementation(() => {
+        return new Promise((resolve, reject) => {
+          resolve({
+            json: () => {
+              return {
+                correlationId: "dummyCorrelationId",
+                questions: [
+                  {
+                    questionKey: "dummyQuestionKey",
+                    info: {},
+                  },
+                  {
+                    questionKey: "dummyQuestionKey1",
+                    info: {
+                      dummyChoice: "dummyChoice",
+                    },
+                  },
+                ],
+              };
+            },
+          });
+        });
+      });
+    });
+
+    describe("Failure Scenarios", async () => {
+      const fetchQuestionsHandler = new FetchQuestionsHandler();
+      const result = await fetchQuestionsHandler.handler(
+        smInput,
+        {} as Context
+      );
+      expect(result).toEqual({
+        correlationId: "dummyCorrelationId",
+        questions: [
+          {
+            questionKey: "dummyQuestionKey",
+            info: {},
+          },
+          {
+            questionKey: "dummyQuestionKey1",
+            info: {
+              dummyChoice: "dummyChoice",
+            },
+          },
+        ],
+      });
+    });
+
+    it("should throw an error when HMRC returns no questions", async () => {
+      global.fetch = jest.fn().mockImplementation(() => {
+        return new Promise((resolve, reject) => {
+          resolve({
+            json: () => {
+              return {
+                correlationId: "dummyCorrelationId",
+                questions: [],
+              };
+            },
+          });
+        });
+      });
+      await expect(() =>
+        new FetchQuestionsHandler().handler(smInput, {} as Context)
+      ).rejects.toThrow("No questions returned");
+    });
+
+    it("should throw an error when HMRC does not return valid JSON", async () => {
+      global.fetch = jest.fn().mockImplementation(() => {
+        return new Promise((resolve, reject) => {
+          resolve({
+            text: () => {
+              return "dummyText";
+            },
+          });
+        });
+      });
+      await expect(() =>
+        new FetchQuestionsHandler().handler(smInput, {} as Context)
+      ).rejects.toThrow("dummyText");
+    });
   });
 });
