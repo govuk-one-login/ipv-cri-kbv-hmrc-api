1b1732c0a2f24fc4d4a75a9b3612e8ee0046fc72
Karol C. Gancarz <karol.sinx@gmail.com>
2023-11-16 18:10:49 +0000

    OJ-1960: Update repo config from the templates repo
    Apply dotfiles from the repo template

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,30 @@
 #!/usr/bin/env bash
-set -e
+set -eu
 
-stack_name="$1"
-common_stack_name="${2:-kbv-hmrc-common-cri-api-local}"
-secret_prefix="${3:-kbv-hmrc-cri-api}"
+stack_name="${1:-}"
 
-if [ -z "$stack_name" ]
-then
-echo "😱 stack name expected as first argument, e.g. ./deploy kbv-hmrc-user1"
-exit 1
+if ! [[ "$stack_name" ]]; then
+  echo "😱 Stack name expected as first argument, e.g. ./deploy.sh pdv-matching-user1"
+  exit 1
 fi
 
-./gradlew clean
 sam validate -t infrastructure/template.yaml
-sam build -t infrastructure/template.yaml
+sam validate -t infrastructure/template.yaml --lint
+
+sam build -t infrastructure/template.yaml --cached --parallel
+
 sam deploy --stack-name "$stack_name" \
-   --no-fail-on-empty-changeset \
-   --no-confirm-changeset \
-   --resolve-s3 \
-   --region eu-west-2 \
-   --capabilities CAPABILITY_IAM \
-   --parameter-overrides \
-   CodeSigningEnabled=false \
-   Environment=dev \
-   AuditEventNamePrefix=/common-cri-parameters/KbvHmrcAuditEventNamePrefix \
-   CriIdentifier=/common-cri-parameters/KbvHmrcCriIdentifier \
-   CommonStackName="$common_stack_name" \
-   SecretPrefix="$secret_prefix"
+  --no-fail-on-empty-changeset \
+  --no-confirm-changeset \
+  --resolve-s3 \
+  --s3-prefix "$stack_name" \
+  --region "${AWS_REGION:-eu-west-2}" \
+  --capabilities CAPABILITY_IAM \
+  --tags \
+  cri:component=check-hmrc-api \
+  cri:stack-type=dev \
+  cri:application=Orange \
+  cri:deployment-source=manual \
+  --parameter-overrides \
+  CodeSigningEnabled=false \
+  Environment=dev
