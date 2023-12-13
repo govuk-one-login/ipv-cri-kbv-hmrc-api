e82a49c15a48e361db393968604f1b294be4a4d6
Karol C. Gancarz <karol.sinx@gmail.com>
2023-12-13 03:54:49 +0000

    BAU: Enable easy deployment to dev
    - Derive stack name from the user's name and the CRI name if the name is not provided to the script
    
    - Add a deploy script to the package.json files so a stack can be quickly deployed from anywhere in the repo by simply running `npm run deploy`

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
+cd "$(dirname "${BASH_SOURCE[0]}")"
 set -eu
 
 stack_name="${1:-}"
 common_stack_name="${2:-}"
 
 if ! [[ "$stack_name" ]]; then
-  echo "😱 Stack name expected as first argument, e.g. ./deploy.sh pdv-matching-user1"
-  exit 1
+  [[ $(aws whoami --query Arn --output text) =~ \/([^\/\.]+)\. ]] && user="${BASH_REMATCH[1]}" || exit
+  stack_name="$user-kbv-hmrc-api"
+  echo "» Using stack name '$stack_name'"
 fi
 
 sam validate -t infrastructure/template.yaml
@@ -22,7 +24,7 @@ sam deploy --stack-name "$stack_name" \
   --region "${AWS_REGION:-eu-west-2}" \
   --capabilities CAPABILITY_IAM \
   --tags \
-  cri:component=kbv-hmrc-api \
+  cri:component=ipv-cri-kbv-hmrc-api \
   cri:stack-type=dev \
   cri:application=Orange \
   cri:deployment-source=manual \
