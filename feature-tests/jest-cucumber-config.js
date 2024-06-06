ee5a652a37a5c529f88c63c1cc406aa49afa525a
Chris Bates <chris.bates@digital.cabinet-office.gov.uk>
2024-06-06 15:47:04 +0100

    initial concept

---
+++ feature-tests/jest-cucumber-config.js
@@ -0,0 +1,6 @@
+const setJestCucumberConfiguration = require('jest-cucumber').setJestCucumberConfiguration;
+
+setJestCucumberConfiguration({
+    tagFilter: process.env.tagFilter,
+    errorOnMissingScenariosAndSteps: false
+  });
