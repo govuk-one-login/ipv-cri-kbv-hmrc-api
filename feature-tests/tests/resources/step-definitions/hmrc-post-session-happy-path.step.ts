0e18960ed4234df1eb9e17f00f17b016cfbaee72
Chris Bates <chris.bates@digital.cabinet-office.gov.uk>
2024-06-13 22:30:13 +0100

    added createRequest Method in hmrc-post-session step file

--- feature-tests/tests/resources/step-definitions/hmrc-post-session-happy-path.step.ts
+++ feature-tests/tests/resources/step-definitions/hmrc-post-session-happy-path.step.ts
@@ -3,7 +3,7 @@ import request from 'supertest';
 import { timeDelayForTestEnvironment } from '../../../utils/utility';
 import EndPoints from '../../../apiEndpoints/endpoints';
 import { SESSION_REQUEST_BODY } from '../../../utils/session_request_body_data';
-
+import { Buffer } from 'node:buffer';
 
 const feature = loadFeature(
   "./tests/resources/features/hmrcPOST/hmrcKbvPostSession-HappyPath.feature"
@@ -11,30 +11,46 @@ const feature = loadFeature(
 
 defineFeature(feature, (test) => {
   let postRequestToSessionEndpoint: any;
-  let sessionRequestBody: any;
-  let getSessionJwtAsJson: any;
+  // let sessionRequestBody: any;
+  // let getSessionJwtAsJson: any;
 
 
 
 // beforeEach(async () => {
 //   });
 
-async function userIdentityAsJwtStringForUpdatedUser(
-  criId: string,
-  updatedJsonString: string,
-): Promise<void> {
+// async function userIdentityAsJwtStringForUpdatedUser(
+//   criId: string,
+//   updatedJsonString: string,
+// ): Promise<void> {
 
 
 
-  // Capture client id for using later in the auth request
-  const CLIENT_ID = ['client_id'];
-  console.log(`CLIENT_ID = ${CLIENT_ID}`);
+// }
+const username = 'user'
+const password = 'bBme2AQ6tdYmP25'
+
+function getformJWT() {
+  const payload = Buffer.from(JSON.stringify(username + ':' + password)).toString('base64url');
+  return `${payload}`;
+}
+
+async function createRequest(user: keyof typeof SESSION_REQUEST_BODY) {
+  console.log(`Initiating set up: creating request containing ${user}`);
+  await timeDelayForTestEnvironment(3500);
+  const response = await request(EndPoints.PRIVATE_API_ENDPOINT + '/backend/createSessionRequest?cri=') 
+    .post(EndPoints.CRI_ID)
+    .send(JSON.stringify(user))
+    .set('Content-Type', 'application/json')
+    .set('Accept', 'application/json')
+    // .set('Authorization', getformJWT)
+    console.log('response from post request: ' + response.statusCode);
+    console.log(JSON.stringify(response));
 }
 
 
 test('Happy Path - Post Request to Session Endpoint', ({
     given,
-    then,
     when,
   }) => {
     given(/^I send a Valid POST request for (.*) to the session endpoint with (.*) and (.*) and (.*)$/, async (user: keyof typeof SESSION_REQUEST_BODY, contentType, accept, xForwardedFor) => {
@@ -43,7 +59,8 @@ test('Happy Path - Post Request to Session Endpoint', ({
   
     postRequestToSessionEndpoint = await request(EndPoints.PRIVATE_API_ENDPOINT)
       .post(EndPoints.SESSION_URL)
-      .send(JSON.stringify(EndPoints.CORE_STUB_URL + 'hmrc-kbv-cri-dev' + user))
+      //JWT encoded JSON Blob
+      .send(JSON.stringify(createRequest))
       .set('Accept', accept)
       .set('X-Forwarded-For', xForwardedFor)
       .set('Content-Type', contentType);
---
+++ feature-tests/utils/create-session.ts
@@ -0,0 +1,4 @@
+import { timeDelayForTestEnvironment } from '../utils/utility';
+import EndPoints from '../apiEndpoints/endpoints';
+
+
