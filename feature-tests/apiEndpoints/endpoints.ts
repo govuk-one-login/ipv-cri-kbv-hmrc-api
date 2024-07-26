5b3c8c4ad7e02ff3d4d4e923ea90eb0a639e8ec8
Jamie Bentham <jamie.bentham@digital.cabinet-office.gov.uk>
2024-07-26 11:24:22 +0100

    LIME-1107: Running tests without docker

--- feature-tests/apiEndpoints/endpoints.ts
+++ feature-tests/apiEndpoints/endpoints.ts
@@ -1,8 +1,5 @@
 export default class EndPoints {
-  static readonly PRIVATE_API_GATEWAY_URL =
-    process.env.TEST_ENVIRONMENT === "dev"
-      ? `https://${process.env.API_GATEWAY_ID_PRIVATE}.execute-api.eu-west-2.amazonaws.com/${process.env.TEST_ENVIRONMENT}`
-      : `https://${process.env.CFN_PrivateApiGatewayId}`;
+  static readonly PRIVATE_API_GATEWAY_URL = `${process.env.PRIVATE_API_GATEWAY}`
   static readonly AUTHORIZATION_URL = "/oauth2/authorize";
   static readonly CORE_STUB_URL = `${process.env.CORE_STUB_URL}`;
   static readonly FETCH_QUESTIONS_ENDPOINT = "/fetchquestions";
--- feature-tests/jest.config.ts
+++ feature-tests/jest.config.ts
@@ -1,23 +1,17 @@
-import type { Config } from "jest";
-import dotenv from "dotenv";
-dotenv.config();
+import type { Config } from 'jest';
 
 const config: Config = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  coveragePathIgnorePatterns: ["/node_modules/"],
-  testMatch: ["**/**/*.step.ts"],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  testMatch: ['**/**/*.step.ts'],
   verbose: true,
   forceExit: true,
-  setupFiles: ["./jest-cucumber-config"],
+  setupFiles: ['./jest-cucumber-config'],
   //coverageReporters: ["html","text"],
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
+    '^.+\\.tsx?$': 'ts-jest',
   },
-  reporters: [
-    "default",
-    ["jest-junit", { outputDirectory: "results", outputName: "report.xml" }],
-  ],
   testTimeout: 100_000,
 };
 export default config;
