fbfb721cd16f4f2906e625e5de5d6697bac58e90
Aaron Henderson <aaron.henderson@digital.cabinet-office.gov.uk>
2024-07-29 15:21:13 +0100

    LIME-1096 Validating answers

---
+++ lambdas/answer-validation/src/answer-validation-handler.ts
@@ -0,0 +1,62 @@
+import { LambdaInterface } from "@aws-lambda-powertools/commons";
+import { Logger } from "@aws-lambda-powertools/logger";
+
+const logger = new Logger({ serviceName: "AnswerValidationHandler" });
+
+export class AnswerValidationHandler implements LambdaInterface {
+  public async handler(event: any, _context: unknown): Promise<object> {
+    let result: boolean = false;
+    if (
+      event.key === "rti-p60-earnings-above-pt" ||
+      event.key === "rti-p60-postgraduate-loan-deductions" ||
+      event.key === "rti-p60-student-loan-deductions" ||
+      event.key === "sa-income-from-pensions"
+    ) {
+      result = poundValidation(event);
+    } else if (event.key === "sa-payment-details") {
+      result = sAPaymentValidation(event);
+    } else {
+      result = penceValidation(event);
+    }
+    logger.info("The answers have been successfully validated");
+    return { validated: result };
+  }
+}
+export function poundValidation(event: any): boolean {
+  if (event.value.match(/^([0-9]{0,12})$/)) {
+    logger.info("Validation in Pounds");
+    return true;
+  } else {
+    logger.info("Validation in Pounds failed");
+    return false;
+  }
+}
+
+export function penceValidation(event: any): boolean {
+  if (event.value.match(/^[0-9]*\.[0-9]{2}$/)) {
+    logger.info("Validation including pence");
+    return true;
+  } else {
+    logger.info("Validation in Pence failed");
+    return false;
+  }
+}
+
+export function sAPaymentValidation(event: any): boolean {
+  if (event.value.length > 200) {
+    logger.info("Input is too long");
+    return false;
+  }
+
+  try {
+    JSON.parse(event.value);
+    logger.info("Parsing Json:");
+    return true;
+  } catch (error) {
+    logger.info(`Parsing Json failed ${error}`);
+    return false;
+  }
+}
+
+const handlerClass = new AnswerValidationHandler();
+export const lambdaHandler = handlerClass.handler.bind(handlerClass);
---
+++ lambdas/answer-validation/tests/answer-validation-handler.test.ts
@@ -0,0 +1,69 @@
+import { AnswerValidationHandler } from "../src/answer-validation-handler";
+
+describe("AnswerValidationHandler", () => {
+  const handler = new AnswerValidationHandler();
+
+  it("should return true for correct pound validation", async () => {
+    const event = {
+      key: "rti-p60-earnings-above-pt",
+      value: "123",
+    };
+    const result = await handler.handler(event, null);
+    expect(result).toEqual({ validated: true });
+  });
+
+  it("should return false for incorrect pound validation", async () => {
+    const event = {
+      key: "sa-income-from-pensions",
+      value: "123.87",
+    };
+    const result = await handler.handler(event, null);
+    expect(result).toEqual({ validated: false });
+  });
+
+  it("should return true for incorrect penny validation", async () => {
+    const event = {
+      key: "rti-payslip-national-insurance",
+      value: "123.87",
+    };
+    const result = await handler.handler(event, null);
+    expect(result).toEqual({ validated: true });
+  });
+
+  it("should return false for incorrect penny validation", async () => {
+    const event = {
+      key: "rti-payslip-income-tax",
+      value: "1238.7",
+    };
+    const result = await handler.handler(event, null);
+    expect(result).toEqual({ validated: false });
+  });
+
+  it("should return false for incorrect penny validation in pounds", async () => {
+    const event = {
+      key: "rti-payslip-income-tax",
+      value: "12387",
+    };
+    const result = await handler.handler(event, null);
+    expect(result).toEqual({ validated: false });
+  });
+
+  it("should return true for correct Self Assessment validation", async () => {
+    const event = {
+      key: "sa-payment-details",
+      value:
+        '{"questionKey": "sa-payment-details","answer": "{\\"amount\\": 300.00,\\"paymentDate\\": \\"2010-01-01\\"}"}',
+    };
+    const result = await handler.handler(event, null);
+    expect(result).toEqual({ validated: true });
+  });
+
+  it("should return false for incorrect Self Assessment validation in pounds", async () => {
+    const event = {
+      key: "sa-payment-details",
+      value: "This is not a Json",
+    };
+    const result = await handler.handler(event, null);
+    expect(result).toEqual({ validated: false });
+  });
+});
